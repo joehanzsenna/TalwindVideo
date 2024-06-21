99220ed4132041211eaf4b70d5ba5f39f2b246aa
joehanzsenna <jonathanedison34@gmail.com>
2024-06-21 01:15:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-[100vw] h-[100vh] bg-lime-500'>
+      <h1 className='text-red-700 text-[4rem]'>Here's some text</h1>
+    </div>
+  )
+}
+
+export default App
